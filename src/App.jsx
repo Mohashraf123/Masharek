3e2d7762f4475cfa6e9fcdf3d644b1cd5873ab66
mohamed <mohashraf359@gmail.com>
2025-03-13 03:22:07 +0200

    update title

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
           <Projects />
           <Footer />
           <footer className="bg-blue-800 text-white text-center p-3">
-            &copy; 2025 مكتب مشارق - جميع الحقوق محفوظة
+            &copy; 2025 منصة مشارق - جميع الحقوق محفوظة
           </footer>
         </>
       )}
